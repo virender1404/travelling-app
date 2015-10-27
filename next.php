426f34e46e84860f58ba2d1784854f484d805e0e
viru <virendrasingh1404@gmail.com>
2015-10-27 14:22:39 +0530

    Update next.php

--- next.php
+++ next.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,66 +13,13 @@
     <script src="https://use.fonticons.com/1ddea501.js"></script>
 	
 
-<style>
-body{
-	background: url('<?php
-include('pass.php');
-$cookie_name ="cook";
-//echo $_COOKIE[$cookie_name]."<br>"; 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-$sql="SELECT * FROM info WHERE place='$_COOKIE[cook]' ";
-$result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
 
-	
-    while($row = $result->fetch_assoc()) {
-
-  	echo $row["background"];
-        
-    }
-   
-
-} else {
-    echo "img/simple.jpg";
-}
-
-$conn->close();
-?> ');
-	-webkit-background-size: cover;
-	-moz-background-size: cover;
-	-o-background-size: cover;
-	background-size: cover;
-}
-</style>
 </head>
 <body>
 
 <br>
 <br>
-
-
-
-
-<div id="div1"><center><h2>welcome</h2>
-
-
-<form action="cook.php" method="post">
-
-place:<br>
-<input type="text" name="place">
-
-<input class="btn btn-primary" type="submit" value="Submit">
-
-</form>
-</center>
-
-</div>
 
 <div class="container-fluid">
 <center>
@@ -77,6 +28,7 @@ place:<br>
 <h2>Group chat</h2>
 <br>
 <p>
+
 <?php
 include('pass.php');
 
@@ -87,32 +39,86 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "SELECT id, msg FROM chat";
+
+
+if(isset($_SESSION['user']))
+{
+	echo " logged in as:         ".$_SESSION["user"];
+	$sql = "SELECT id, msg  ,name ,side FROM chat";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo $row["msg"]. "<br>";
+    	
+        echo "<html><div class='container-fluid' style='width:30%;height:30px;border:1px solid black;'>".$row['msg']."-------  ".$row['name']."
+        </div>
+        <html>";
+       
     }
 } else {
     echo "";
 }
+
+}
 $conn->close();
 ?>
 </p>
-  <div class="row">
+</body>
+</html>
+
+<?php
+if(isset($_SESSION['user']))
+{
+	echo '<html>
+<head>
+	
+</head>
+<body>
+<div class="row">
     <form action="s.php" method="post">
 
 
 <input type="text" name="chat">
 
-<input class="btn btn-primary" type="submit" value="Submit">
+<input class="btn btn-primary" type="submit" value="SEND">
 </form></center>
   </div>
-  
+  <center>
+  <div id="div1" style="float:center;" >
+  <br>
+  <br>
+
+
+ <form action="logout.php" method="post">
+<input type="text" name="logout" value="1"   hidden>
+
+<input class="btn btn-primary" type="submit" value="logout">
+</form>
+
 </div>
+</center></div>
+
+</body>
+</html>';
+  
+}
+?>
+<?php
+if(!isset($_SESSION['user']))
+{
+	echo'<html>
+<body>
+<div><form action="sess.php" method="post">
+<h2>login using name</h2><br>
+<input type="text" name="name">
+
+<input class="btn btn-primary" type="submit" value="log IN">
+</form></div>
 </body>
 </html>
+';
+}
+?>
 
 
